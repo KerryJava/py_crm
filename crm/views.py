dcbda88f083fe9e16969d3eff5645b0101447e5d
jetnet <qq471723562@126.com>
2018-06-22 17:18:39 +0800

    change login style

--- crm/views.py
+++ crm/views.py
@@ -26,7 +26,7 @@ def acc_login(request):
         else:
             error_msg = '用户名或密码错误！'
 
-    return render(request, 'login.html', {'error_msg': error_msg})
+    return render(request, 'login.html', {'error_msg': error_msg, 'app_title': "mojicai system"})
 
 
 @login_required
